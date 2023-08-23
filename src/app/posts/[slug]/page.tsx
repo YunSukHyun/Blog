68a4beda6b6be40506e2f132ac3d1af481858409
YunSukHyun <czcz9207@gmail.com>
2023-08-23 20:29:53 +0900

    feat: MarkdownViewer

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { getPostData } from "@/service/posts";
-import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import Image from "next/image";
+import { AiTwotoneCalendar } from "react-icons/ai";
+import { MarkdownViewer } from "@/components/MarkdownViewer";
 
 type Props = {
   params: {
@@ -8,11 +10,27 @@ type Props = {
 };
 
 const PostPage = async ({ params: { slug } }: Props) => {
-  const post = await getPostData(slug);
+  const { title, description, date, path, content } = await getPostData(slug);
   return (
     <>
-      <h1>{post.title}</h1>
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <article className="rounded-xl overflow-hidden dark:bg-gray-900 bg-gray-100 shadow-lg m-2">
+        <Image
+          className="w-full h-1/6"
+          src={`/images/posts/${path}.png`}
+          alt="title"
+          width={760}
+          height={420}
+        />
+        <section className="flex flex-col p-4">
+          <div className="flex items-center self-end text-sky-600">
+            <AiTwotoneCalendar />
+            <p className="font-semibold ml-1">{date.toString()}</p>
+          </div>
+          <h1 className="text-4xl">{title}</h1>
+          <p className="text-4xl font-bold">{description}</p>
+          <MarkdownViewer content={content} />
+        </section>
+      </article>
     </>
   );
 };
---
+++ src/components/MarkdownViewer.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Image from "next/image";
+import remarkGfm from "remark-gfm";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+export const MarkdownViewer = ({ content }: { content: string }) => {
+  return (
+    <ReactMarkdown
+      className="prose"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <SyntaxHighlighter
+              {...props}
+              style={materialDark}
+              language={match[1]}
+              PreTag="div"
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          );
+        },
+        img: (image) => (
+          <Image
+            className="w-full max-h-80 object-cover"
+            src={image.src || ""}
+            alt={image.alt || ""}
+            width={500}
+            height={350}
+          />
+        ),
+      }}
+    >
+      {content}
+    </ReactMarkdown>
+  );
+};
